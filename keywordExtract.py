1f8bb12ff8324afe7b9429bd8a816ca81260a859
Zhijian-Mei <46204799+Zhijian-Mei@users.noreply.github.com>
2022-04-22 17:43:03 +0800

    Update keywordExtract.py

--- keywordExtract.py
+++ keywordExtract.py
@@ -1,2 +1,8 @@
 import spacy
-model = spacy.load("en_core_web_trf")
+import scispacy
+nlp = spacy.load("en_core_sci_scibert")
+text = """spaCy is an open-source software library for advanced natural language processing, 
+written in the programming languages Python and Cython. The library is published under the MIT license
+and its main developers are Matthew Honnibal and Ines Montani, the founders of the software company Explosion."""
+doc = nlp(text)
+print(doc.ents)
