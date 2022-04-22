673a4350d749bc82464db85b81c737c603af9f38
Zhijian-Mei <46204799+Zhijian-Mei@users.noreply.github.com>
2022-04-22 17:50:03 +0800

    Update keywordExtract.py

--- keywordExtract.py
+++ keywordExtract.py
@@ -1,11 +1,22 @@
 import spacy
 import scispacy
 nlp = spacy.load("en_core_sci_scibert")
-text = """Myeloid derived suppressor cells (MDSC) are immature 
-myeloid cells with immunosuppressive activity. 
-They accumulate in tumor-bearing mice and humans 
-with different types of cancer, including hepatocellular 
-carcinoma (HCC)."""
+text = """Asthma, allergic rhinitis, food allergy, and atopic dermatitis are common childhood diseases
+with several different underlying mechanisms, i.e., endotypes of disease. Metabolomics has the
+potential to identify disease endotypes, which could beneficially promote personalized prevention
+and treatment. Here, we summarize the findings from metabolomics studies of children with
+atopic diseases focusing on tyrosine and tryptophan metabolism, lipids (particularly, sphingolipids),
+polyunsaturated fatty acids, microbially derived metabolites (particularly, short-chain fatty acids),
+and bile acids. We included 25 studies: 23 examined asthma or wheezing, five examined allergy
+endpoints, and two focused on atopic dermatitis. Of the 25 studies, 20 reported findings in the pathways
+of interest with findings for asthma in all pathways and for allergy and atopic dermatitis in most
+pathways except tyrosine metabolism and short-chain fatty acids, respectively. Particularly, tyrosine,
+3-hydroxyphenylacetic acid, N-acetyltyrosine, tryptophan, indolelactic acid, 5-hydroxyindoleacetic
+acid, p-Cresol sulfate, taurocholic acid, taurochenodeoxycholic acid, glycohyocholic acid, glycocholic
+acid, and docosapentaenoate n-6 were identified in at least two studies. This pathway-specific review
+provides a comprehensive overview of the existing evidence from metabolomics studies of childhood
+atopic diseases. The altered metabolic pathways uncover some of the underlying biochemical
+mechanisms leading to these common childhood disorders, which may become of potential value in
+clinical practice."""
 doc = nlp(text)
 print(doc.ents)
-quit()
