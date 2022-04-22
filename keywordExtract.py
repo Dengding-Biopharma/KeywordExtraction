9110a6405eb7903b1a2778b2c106a70829529ba8
Zhijian-Mei <46204799+Zhijian-Mei@users.noreply.github.com>
2022-04-22 17:48:34 +0800

    Update keywordExtract.py

--- keywordExtract.py
+++ keywordExtract.py
@@ -1,12 +1,11 @@
 import spacy
 import scispacy
 nlp = spacy.load("en_core_sci_scibert")
-print(nlp)
-quit()
 text = """Myeloid derived suppressor cells (MDSC) are immature 
 myeloid cells with immunosuppressive activity. 
 They accumulate in tumor-bearing mice and humans 
 with different types of cancer, including hepatocellular 
 carcinoma (HCC)."""
 doc = nlp(text)
-print(doc.ents)
+print(doc.tensor)
+quit()
