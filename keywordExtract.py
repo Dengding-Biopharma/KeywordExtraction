2cc396e4fc53f90df1a6aeeb95d440c90e3a51dc
Zhijian-Mei <46204799+Zhijian-Mei@users.noreply.github.com>
2022-04-26 11:30:09 +0800

    Update keywordExtract.py

--- keywordExtract.py
+++ keywordExtract.py
@@ -19,4 +19,7 @@ atopic diseases. The altered metabolic pathways uncover some of the underlying b
 mechanisms leading to these common childhood disorders, which may become of potential value in
 clinical practice."""
 doc = nlp(text)
-print(doc.ents)
+
+print("TEXT", "START", "END", "ENTITY TYPE")
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
